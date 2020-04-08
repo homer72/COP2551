06ec7088027a2881127047fda088b97c31a53ade
homer72 <51171262+homer72@users.noreply.github.com>
2020-04-08 19:53:42 -0400

    Add files via upload

---
+++ ClassProject.java
@@ -0,0 +1,8 @@
+
+//Usually you will require both swing and awt packages
+// even if you are working with just swings.
+class ClassProject {
+    public static void main(String args[]) {
+    	GUI window = new GUI();
+    }
+}
